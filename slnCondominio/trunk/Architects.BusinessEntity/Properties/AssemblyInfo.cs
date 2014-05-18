0a562df6f5d0e762854c18e46844f31332b8b5ba
omcr1905 <omcr1905@gmail.com>
2014-05-18 18:42:36 +0000

    Implementado el acceso a datos con el objeto DBMCL

--- slnCondominio/trunk/Architects.BusinessEntity/Properties/AssemblyInfo.cs
+++ slnCondominio/trunk/Architects.BusinessEntity/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Architects.BusinessEntity")]
+[assembly: AssemblyTitle("Architects.Dominio")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Architects.BusinessEntity")]
+[assembly: AssemblyProduct("Architects.Dominio")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
