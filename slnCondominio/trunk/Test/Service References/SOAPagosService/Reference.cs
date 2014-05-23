9d0224d4c8aa6d1f0aa67c954bfdb884f59faaa8
omcr1905 <omcr1905@gmail.com>
2014-05-23 12:19:10 +0000

    Test de Cuota

--- slnCondominio/trunk/Test/Service References/SOAPagosService/Reference.cs
+++ slnCondominio/trunk/Test/Service References/SOAPagosService/Reference.cs
@@ -9,43 +9,142 @@
 //------------------------------------------------------------------------------
 
 namespace Test.SOAPagosService {
+    using System.Runtime.Serialization;
+    using System;
     
     
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
-    [System.ServiceModel.ServiceContractAttribute(ConfigurationName="SOAPagosService.ICuotaService")]
-    public interface ICuotaService {
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
+    [System.Runtime.Serialization.DataContractAttribute(Name="RetornaMensaje", Namespace="http://schemas.datacontract.org/2004/07/WcfService")]
+    [System.SerializableAttribute()]
+    public partial class RetornaMensaje : object, System.Runtime.Serialization.IExtensibleDataObject, System.ComponentModel.INotifyPropertyChanged {
         
-        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/ICuotaService/ListarTipoPago", ReplyAction="http://tempuri.org/ICuotaService/ListarTipoPagoResponse")]
-        Architects.Dominio.TipoPago[] ListarTipoPago();
+        [System.NonSerializedAttribute()]
+        private System.Runtime.Serialization.ExtensionDataObject extensionDataField;
         
-        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/ICuotaService/CrearTipoPago", ReplyAction="http://tempuri.org/ICuotaService/CrearTipoPagoResponse")]
-        Architects.Dominio.TipoPago CrearTipoPago(string Descripcion);
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private string CodigoErrorField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private int CodigoRetornoField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private bool ExitoField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private string MensageField;
+        
+        [global::System.ComponentModel.BrowsableAttribute(false)]
+        public System.Runtime.Serialization.ExtensionDataObject ExtensionData {
+            get {
+                return this.extensionDataField;
+            }
+            set {
+                this.extensionDataField = value;
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public string CodigoError {
+            get {
+                return this.CodigoErrorField;
+            }
+            set {
+                if ((object.ReferenceEquals(this.CodigoErrorField, value) != true)) {
+                    this.CodigoErrorField = value;
+                    this.RaisePropertyChanged("CodigoError");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public int CodigoRetorno {
+            get {
+                return this.CodigoRetornoField;
+            }
+            set {
+                if ((this.CodigoRetornoField.Equals(value) != true)) {
+                    this.CodigoRetornoField = value;
+                    this.RaisePropertyChanged("CodigoRetorno");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public bool Exito {
+            get {
+                return this.ExitoField;
+            }
+            set {
+                if ((this.ExitoField.Equals(value) != true)) {
+                    this.ExitoField = value;
+                    this.RaisePropertyChanged("Exito");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public string Mensage {
+            get {
+                return this.MensageField;
+            }
+            set {
+                if ((object.ReferenceEquals(this.MensageField, value) != true)) {
+                    this.MensageField = value;
+                    this.RaisePropertyChanged("Mensage");
+                }
+            }
+        }
+        
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        
+        protected void RaisePropertyChanged(string propertyName) {
+            System.ComponentModel.PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if ((propertyChanged != null)) {
+                propertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
     
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
-    public interface ICuotaServiceChannel : Test.SOAPagosService.ICuotaService, System.ServiceModel.IClientChannel {
+    [System.ServiceModel.ServiceContractAttribute(ConfigurationName="SOAPagosService.IPagosService")]
+    public interface IPagosService {
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPagosService/ListarTipoPago", ReplyAction="http://tempuri.org/IPagosService/ListarTipoPagoResponse")]
+        Architects.Dominio.TipoPago[] ListarTipoPago();
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPagosService/RegistrarTipoPago", ReplyAction="http://tempuri.org/IPagosService/RegistrarTipoPagoResponse")]
+        Architects.Dominio.TipoPago RegistrarTipoPago(string Descripcion);
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPagosService/RegistrarCuota", ReplyAction="http://tempuri.org/IPagosService/RegistrarCuotaResponse")]
+        [System.ServiceModel.FaultContractAttribute(typeof(Test.SOAPagosService.RetornaMensaje), Action="http://tempuri.org/IPagosService/RegistrarCuotaRetornaMensajeFault", Name="RetornaMensaje", Namespace="http://schemas.datacontract.org/2004/07/WcfService")]
+        Test.SOAPagosService.RetornaMensaje RegistrarCuota(string C_Periodo, int N_IdVivienda, int N_IdTipoPago, decimal N_Importe, System.DateTime D_FecVncto);
+    }
+    
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    public interface IPagosServiceChannel : Test.SOAPagosService.IPagosService, System.ServiceModel.IClientChannel {
     }
     
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
-    public partial class CuotaServiceClient : System.ServiceModel.ClientBase<Test.SOAPagosService.ICuotaService>, Test.SOAPagosService.ICuotaService {
+    public partial class PagosServiceClient : System.ServiceModel.ClientBase<Test.SOAPagosService.IPagosService>, Test.SOAPagosService.IPagosService {
         
-        public CuotaServiceClient() {
+        public PagosServiceClient() {
         }
         
-        public CuotaServiceClient(string endpointConfigurationName) : 
+        public PagosServiceClient(string endpointConfigurationName) : 
                 base(endpointConfigurationName) {
         }
         
-        public CuotaServiceClient(string endpointConfigurationName, string remoteAddress) : 
+        public PagosServiceClient(string endpointConfigurationName, string remoteAddress) : 
                 base(endpointConfigurationName, remoteAddress) {
         }
         
-        public CuotaServiceClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) : 
+        public PagosServiceClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) : 
                 base(endpointConfigurationName, remoteAddress) {
         }
         
-        public CuotaServiceClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) : 
+        public PagosServiceClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) : 
                 base(binding, remoteAddress) {
         }
         
@@ -53,8 +152,12 @@ namespace Test.SOAPagosService {
             return base.Channel.ListarTipoPago();
         }
         
-        public Architects.Dominio.TipoPago CrearTipoPago(string Descripcion) {
-            return base.Channel.CrearTipoPago(Descripcion);
+        public Architects.Dominio.TipoPago RegistrarTipoPago(string Descripcion) {
+            return base.Channel.RegistrarTipoPago(Descripcion);
+        }
+        
+        public Test.SOAPagosService.RetornaMensaje RegistrarCuota(string C_Periodo, int N_IdVivienda, int N_IdTipoPago, decimal N_Importe, System.DateTime D_FecVncto) {
+            return base.Channel.RegistrarCuota(C_Periodo, N_IdVivienda, N_IdTipoPago, N_Importe, D_FecVncto);
         }
     }
 }
---
+++ slnCondominio/trunk/Test/TestPagos.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Architects.Dominio;
+using Test.SOAPagosService;
+using System.ServiceModel;
+
+namespace Test
+{
+    [TestClass]
+    public class TestPagos
+    {
+
+        [TestMethod]
+        public void TipoPagoListarTest()
+        {
+            SOAPagosService.PagosServiceClient proxy = new SOAPagosService.PagosServiceClient();
+            IList<TipoPago> tipoPagoListar;
+            tipoPagoListar = proxy.ListarTipoPago();
+
+            Assert.AreNotEqual(tipoPagoListar.Count, 0);
+        }
+
+        [TestMethod]
+        public void TipoPagoCrearTest()
+        {
+            SOAPagosService.PagosServiceClient proxy = new SOAPagosService.PagosServiceClient();
+            TipoPago tipoPago = new TipoPago();
+            string strDescripcion = "CONTADO";
+            tipoPago = proxy.RegistrarTipoPago(strDescripcion);
+
+            Assert.AreEqual(tipoPago.C_Descripcion, strDescripcion);
+            /* */
+        }
+
+        [TestMethod]
+        public void RegistrarCuotaTest()
+        {
+            SOAPagosService.PagosServiceClient proxy = new SOAPagosService.PagosServiceClient();
+            RetornaMensaje retornaMensaje = null;
+            Cuota cuotaRegistrada = new Cuota();
+
+            string C_Periodo = "201401"; 
+            int N_IdVivienda=1; 
+            int N_IdTipoPago=1; 
+            decimal N_Importe = 120; 
+            DateTime D_FecVncto = DateTime.Now;
+            try
+            {
+                retornaMensaje = proxy.RegistrarCuota(C_Periodo, N_IdVivienda, N_IdTipoPago, N_Importe, D_FecVncto);
+
+                Assert.AreNotEqual(0, retornaMensaje.CodigoRetorno);
+            }
+            catch (FaultException<RetornaMensaje> exception)
+            {
+                Console.WriteLine("Error : {0}", exception.Detail.Mensage);
+            }
+            
+            
+        }
+
+    }
+}
