56ea5a4a671f303d9c1b32d04f8fb9fb9e1d2bf5
justindelabarra@gmail.com <justindelabarra@gmail.com@80b915e4-f584-5d82-7bb2-b4c29f7e1674>
2014-05-26 20:21:30 +0000

    Subiendo el Servicio de Residentes

---
+++ trunk/ARCHITECTSCONDOMINIO/Architecs.ResidentesService/Persistencia/ResidenteDA.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Data.Common;
+using Architects.Dominio;
+
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using Microsoft.Practices.EnterpriseLibrary.Common;
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+
+namespace Architects.Persistencia
+{
+    public class ResidenteDA : IResidenteDA
+    {
+        private Database _db = null;
+
+        public ResidenteDA()
+        {
+            _db = EnterpriseLibraryContainer.Current.GetInstance<Database>("ConnDB");
+        }
+
+        public ResidenteBE[] ListarResidentes()
+        {
+            List<ResidenteBE> Residentes = new List<ResidenteBE>();
+            ResidenteBE residente = null;
+
+            try
+            {
+                using (IDataReader dataReader = _db.ExecuteReader("Maestros.USP_LIST_RESIDENTE"))
+                {
+                    while (dataReader.Read())
+                    {
+                        residente = new ResidenteBE();
+
+                        residente.N_IdResidente = dataReader.GetInt32(0);
+                        residente.C_Nombre = dataReader.GetString(1);
+                        residente.C_Apellidos = dataReader.GetString(2);
+                        residente.N_TipoDoc = dataReader.GetInt32(3);
+                        residente.C_NumDocume = dataReader.GetString(4);
+                        residente.D_FecNacimi = dataReader.GetDateTime(5);
+                        residente.C_Correo = dataReader.IsDBNull(6) ? "" : dataReader.GetString(6);
+                        residente.B_Estado = dataReader.GetBoolean(7);
+
+                        Residentes.Add(residente);
+                    }
+                }
+
+                return Residentes.ToArray();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Int32 CrearResidente(ResidenteBE prmResidente)
+        {
+            DbCommand insertCommand = null;            
+
+            try
+            {
+                insertCommand = _db.GetStoredProcCommand("Maestros.USP_INSERT_RESIDENTE");
+                _db.AddInParameter(insertCommand, "@C_Nombre", DbType.String, prmResidente.C_Nombre);
+                _db.AddInParameter(insertCommand, "@C_Apellidos", DbType.String, prmResidente.C_Apellidos);
+                _db.AddInParameter(insertCommand, "@N_TipoDoc", DbType.Int32, prmResidente.N_TipoDoc);
+                _db.AddInParameter(insertCommand, "@C_NumDocume", DbType.String, prmResidente.C_NumDocume);
+                _db.AddInParameter(insertCommand, "@D_FecNacimi", DbType.DateTime, prmResidente.D_FecNacimi);
+                _db.AddInParameter(insertCommand, "@C_Correo", DbType.String, prmResidente.C_Correo);
+                _db.AddInParameter(insertCommand, "@C_Clave", DbType.String, prmResidente.C_Clave);
+
+                var result = _db.ExecuteScalar(insertCommand);
+
+                return Int32.Parse(result.ToString());
+            }
+            catch (Exception ex)
+            {                
+                throw ex;
+            }
+        }
+
+        public void ActualizarResidente(ResidenteBE prmResidente)
+        {
+            DbCommand updateCommand = null;
+
+            try
+            {
+                updateCommand = _db.GetStoredProcCommand("Maestros.USP_UPDATE_RESIDENTE");
+                _db.AddInParameter(updateCommand, "@N_IdResidente", DbType.Int32, prmResidente.N_IdResidente);
+                _db.AddInParameter(updateCommand, "@C_Nombre", DbType.String, prmResidente.C_Nombre);
+                _db.AddInParameter(updateCommand, "@C_Apellidos", DbType.String, prmResidente.C_Apellidos);
+                _db.AddInParameter(updateCommand, "@N_TipoDoc", DbType.Int32, prmResidente.N_TipoDoc);
+                _db.AddInParameter(updateCommand, "@C_NumDocume", DbType.String, prmResidente.C_NumDocume);
+                _db.AddInParameter(updateCommand, "@D_FecNacimi", DbType.DateTime, prmResidente.D_FecNacimi);
+                _db.AddInParameter(updateCommand, "@C_Correo", DbType.String, prmResidente.C_Correo);                
+
+                _db.ExecuteNonQuery(updateCommand);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        
+        public void EliminarResidente(int residenteID)
+        {
+            DbCommand deleteCommand = null;
+
+            try
+            {
+                deleteCommand = _db.GetStoredProcCommand("Maestros.USP_DELETE_RESIDENTE");
+                _db.AddInParameter(deleteCommand, "@pi_IdResidente", DbType.Int32, residenteID);
+                _db.ExecuteNonQuery(deleteCommand);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ trunk/ARCHITECTSCONDOMINIO/Architecs.ResidentesService/ResidenteService.svc.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+using Architects.Dominio;
+using Architects.Persistencia;
+
+namespace ResidenteService
+{    
+    public class ResidenteService : IResidenteService
+    {
+        IResidenteDA objDA = new ResidenteDA();
+        ValidationException vExc = null;
+        
+        public ResidenteBE[] ListarResidentes()
+        {
+            try
+            {
+                return objDA.ListarResidentes();
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException<ValidationException>
+                    (new ValidationException { ValidationError = ex.Message }, 
+                    new FaultReason("Validation Failed"));
+            }
+        }
+
+        public IEnumerable<ResidenteBE> ListarResidentesPaginado(int page, int size)
+        {
+            List<ResidenteBE> listaResidentes = null;
+
+            try
+            {
+                listaResidentes = new List<ResidenteBE>(ListarResidentes());
+
+                return listaResidentes.OrderBy(c => c.N_IdResidente).Skip(page * size).Take(size);
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException<ValidationException>
+                    (new ValidationException { ValidationError = ex.Message },
+                    new FaultReason("Validation Failed"));
+            }         
+        }
+
+        public ResidenteBE ObtenerResidentePorID(int residenteId)
+        {
+            List<ResidenteBE> listaResidentes = null;
+            ResidenteBE result = null;
+            
+            try
+            {
+                listaResidentes = new List<ResidenteBE>(ListarResidentes());
+
+                var nres = from l in listaResidentes
+                          where l.N_IdResidente == residenteId
+                          select l;
+
+                foreach (var item in nres)
+                {
+                    result = new ResidenteBE();
+                    result.N_IdResidente = item.N_IdResidente;
+                    result.C_Nombre = item.C_Nombre;
+                    result.C_Apellidos = item.C_Apellidos;
+                    result.B_Estado = item.B_Estado;
+                    result.N_TipoDoc = item.N_TipoDoc;
+                    result.C_NumDocume = item.C_NumDocume;
+                    result.D_FecNacimi = item.D_FecNacimi;
+                    result.C_Correo = item.C_Correo;                    
+                }
+                
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException<ValidationException>
+                    (new ValidationException { ValidationError = ex.Message },
+                    new FaultReason("Validation Failed"));
+            }
+        }
+
+        public int CrearResidente(ResidenteBE prmResidente)
+        {
+            try
+            {
+                return objDA.CrearResidente(prmResidente);
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException<ValidationException>
+                    (new ValidationException { ValidationError = ex.Message },
+                    new FaultReason("Validation Failed"));
+            }
+        }
+
+        public ValidationException ActualizarResidente(ResidenteBE prmResidente)
+        {
+            vExc = new ValidationException();
+
+            try
+            {
+                objDA.ActualizarResidente(prmResidente);
+                vExc.ValidationError = string.Format(resMensajes.msjGuardadoOK, "Residente");
+                return vExc;
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException<ValidationException>
+                    (new ValidationException { ValidationError = string.Format(resMensajes.msjNoRegistrado, "Residente") },
+                    new FaultReason(ex.Message));
+            }
+        }
+
+        public ValidationException EliminarResidente(int residenteID)
+        {
+            vExc = new ValidationException();
+
+            try
+            {
+                objDA.EliminarResidente(residenteID);
+                vExc.ValidationError = string.Format(resMensajes.msjEliminadoOK, "Residente");
+                return vExc;
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException<ValidationException>
+                    (new ValidationException { ValidationError = string.Format(resMensajes.msjNoEliminado, "Residente") },
+                    new FaultReason(ex.Message));
+            }
+        }
+    }
+}
