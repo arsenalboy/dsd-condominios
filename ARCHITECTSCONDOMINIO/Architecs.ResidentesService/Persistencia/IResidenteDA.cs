c4a27415c91666c51043ca028ecd0f7f74140a9b
justindelabarra <justindelabarra@gmail.com>
2014-05-26 23:32:44 +0000

    Search method

--- ARCHITECTSCONDOMINIO/Architecs.ResidentesService/Persistencia/IResidenteDA.cs
+++ ARCHITECTSCONDOMINIO/Architecs.ResidentesService/Persistencia/IResidenteDA.cs
@@ -9,7 +9,8 @@ namespace Architects.Persistencia
 {
     public interface IResidenteDA
     {
-        ResidenteBE[] ListarResidentes();        
+        ResidenteBE[] ListarResidentes();
+        ResidenteBE[] BuscarResidentes(string nombre, string apellidos,  string numDocumento);
         Int32 CrearResidente(ResidenteBE prmResidente);
         void ActualizarResidente(ResidenteBE prmResidente);
         void EliminarResidente(int residenteID);
