aaccfd4efbdc2d7c6157aa604c1ce7cc1d1453f4
acantuariasd <acantuariasd@80b915e4-f584-5d82-7bb2-b4c29f7e1674>
2014-05-29 07:45:24 +0000

    WS Vivienda

---
+++ trunk/ARCHITECTSCONDOMINIO/Architecs.ViviendasService/Persistencia/Utilitario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Architecs.ViviendasService.Persistencia
+{
+    public static class Utilitario
+    {
+        public static string CadenaConeccion()
+        {
+            return "workstation id=CondominiosDB.mssql.somee.com;packet size=4096;user id=julioanyosa_SQLLogin_1;pwd=spnnkydfl1;data source=CondominiosDB.mssql.somee.com;persist security info=False;initial catalog=CondominiosDB";
+        }
+    }
+}
