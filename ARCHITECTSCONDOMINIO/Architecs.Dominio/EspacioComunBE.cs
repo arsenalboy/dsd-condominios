7975e341136ef79bffc88917289cbe8eeb1b54c8
llp.lozano <llp.lozano@gmail.com>
2014-05-29 21:58:30 +0000

    Creación de Servicio de Horarios y Espacio Comun

---
+++ ARCHITECTSCONDOMINIO/Architecs.Dominio/EspacioComunBE.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Architects.Dominio
+{
+    class EspacioComunBE
+    {
+        public int N_IdEspacio { get; set; }
+        public string C_Nombre { get; set; }
+        public int B_Estado { get; set; }
+    }
+}
