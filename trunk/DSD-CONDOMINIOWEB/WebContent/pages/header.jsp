ef30e84036d7757ada3695d4c23f4f767f2894b3
omcr1905 <omcr1905@gmail.com>
2014-05-29 12:37:02 +0000

    Implementado el modulo de cuotas con WCF

--- trunk/DSD-CONDOMINIOWEB/WebContent/pages/header.jsp
+++ trunk/DSD-CONDOMINIOWEB/WebContent/pages/header.jsp
@@ -38,7 +38,7 @@
 			  <li><a href="<%=request.getContextPath()%>/pages/ingresarReserva.jsp?x=0" target="_top">Reservas</a></li>
 			  <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Cuotas <b class="caret"></b></a>
 			  		<ul class="dropdown-menu">
-						<li><a href="<%=request.getContextPath()%>/pages/frmCuotaListar.jsp" target="_top" >Listar</a></li>
+						<li><a href="<%=request.getContextPath()%>/CuotaServlet?op=list" target="_top" >Listar</a></li>
 						<li><a href="<%=request.getContextPath()%>/pages/frmCuotaCrear.jsp?x=0" target="_top" >Registrar</a></li>
 						<li><a href="<%=request.getContextPath()%>/pages/frmCuotaPagos.jsp" target="_top" >Listar Pagos</a></li>	     
 						<li><a href="<%=request.getContextPath()%>/pages/frmCuotaPagar.jsp?x=0" target="_top" >Realizar Pago</a></li>   							
--- trunk/DSD-CONDOMINIOWEB/src/servlets/CuotaServlet.java
+++ trunk/DSD-CONDOMINIOWEB/src/servlets/CuotaServlet.java
@@ -1,18 +1,24 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.swing.text.html.parser.Parser;
 
+import org.apache.catalina.connector.Request;
+import org.apache.jasper.tagplugins.jstl.core.Out;
 import org.datacontract.schemas._2004._07.Architecs_PagosService.Cuota;
 import org.datacontract.schemas._2004._07.Architecs_PagosService.TipoPago;
+import org.datacontract.schemas._2004._07.Architects_Dominio.ResidenteBE;
 
 import util.FormatoFecha;
 
@@ -29,46 +35,65 @@ public class CuotaServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		processRequest(request,response);
+		//this.listar(request, response);
+		//Video Tutorial: https://www.youtube.com/watch?v=Uo-OnVxnYiU
+		//https://www.youtube.com/watch?v=E6cfGqqKegI
+		
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		processRequest(request,response);
+		//String op= request.getParameter("op");
+		//String pagina ;
+		//if(op.equals("create"))
+	//	{
+		//	pagina= "/pages/frmCuotaCrear.jsp";
+		//	//RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagina);
+	//		//dispatcher.forward(request, response);
+	//		request.getRequestDispatcher(pagina).forward(request, response);
+//		}
+	//	else{
+			this.listar(request, response);
+	//	}
 	}
 	
 	private void processRequest(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		String opcion = request.getParameter("opcion")==null?"2":request.getParameter("opcion");
-		String periodo =request.getParameter("txtperiodo"); 
 		
-		String pPeriodo =request.getParameter("periodo"); 
-		int pIdVivienda = Integer.parseInt(request.getParameter("slcvivienda")==null?"2":request.getParameter("slcvivienda"));
+		String op= request.getParameter("op");
+		String pagina="";
+		if(op.equals("create"))
+		{
+			//RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagina);
+			//dispatcher.forward(request, response);
+			pagina= "/pages/frmCuotaCrear.jsp";
+			request.getRequestDispatcher(pagina).forward(request, response);
+			
+		}
+		else{
+			this.listar(request, response);
+		}
+		//this.listar(request, response);
 		
-		//int pIdTipoPago =request.getParameter("txtperiodo"); 
-		double pImporte =Double.parseDouble(request.getParameter("importepago")); 
-		Calendar pFecVncto = FormatoFecha.stringToCalendarDate(request.getParameter("fechaVcto")==null?"2":request.getParameter("fechaVcto"));
+	}
+
+	private void listar(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		
-		
-		//System.out.println("<get> paramOpcion :"+periodo+ "</h3>");
-		
-		String page="";	
+		String page=""; 
 		try{
-			if(opcion.equals("1")){	//trae todos las cuotas
-				
-				PagosWS cuota = new PagosWS();
-				
-				Cuota[] lstCuota = cuota.ListarCuota(periodo);
-				request.setAttribute("lstCuotas", lstCuota);
-				page="/pages/frmCuotaListar.jsp";
-				
-			}else if(opcion.equals("2")){	//trae todos las cuotas
-				PagosWS cuota = new PagosWS();
-				//cuota.RegistrarCuota(pPeriodo, pIdVivienda, pImporte, pFecVncto);
-			}
+
+			PagosWS cuota = new PagosWS();
+			
+			Cuota[] lstCuota = cuota.ListarCuota("");
+			request.setAttribute("lstCuotas", lstCuota);
+			page="/pages/frmCuotaListar.jsp";
+			
+			
 		}catch(Exception e){
 			request.setAttribute("mensaje",e.getMessage());	
 		}
-		
 		request.getRequestDispatcher(page).forward(request, response);
 	}
-
 }
