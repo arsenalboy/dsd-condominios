9556713067d01f71ca9b99e2ede7c30671acfeff
llp.lozano@gmail.com <llp.lozano@gmail.com@80b915e4-f584-5d82-7bb2-b4c29f7e1674>
2014-05-29 21:58:30 +0000

    Creación de Servicio de Horarios y Espacio Comun

---
+++ trunk/ARCHITECTSCONDOMINIO/Architecs.Dominio/HorarioBE.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Architects.Dominio
+{
+    class HorarioBE
+    {
+        public int N_IdHorario { get; set; }
+        public string C_Rango { get; set; }
+        public int B_Estado { get; set; }
+    }
+}
---
+++ trunk/ARCHITECTSCONDOMINIO/Architecs.HorariosService/Persitencia/HorarioDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Architects.Dominio;
+
+namespace Architecs.HorariosService.Persitencia
+{
+    public class HorarioDAO
+    {
+       
+        
+    }
+}
