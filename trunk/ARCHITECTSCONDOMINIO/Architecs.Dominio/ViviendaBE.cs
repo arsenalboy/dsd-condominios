42f6a5a18294d42c37c1551244bcd2f5c1cb7783
acantuariasd <acantuariasd@80b915e4-f584-5d82-7bb2-b4c29f7e1674>
2014-05-29 07:46:06 +0000

    Se creó la entidad

---
+++ trunk/ARCHITECTSCONDOMINIO/Architecs.Dominio/ViviendaBE.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Architects.Dominio
+{
+    public class ViviendaBE
+    {
+        public ViviendaBE()
+        {
+            objResidente = new ResidenteBE();
+        }
+
+        [DataMember]
+        public int N_IdVivienda { get; set; }
+        [DataMember]
+        public int N_IdResidente { get; set; }
+        [DataMember]
+        public string C_NumEdificio { get; set; }
+        [DataMember]
+        public string C_NumDpto { get; set; }
+        [DataMember]
+        public decimal N_NumMetros { get; set; }
+        [DataMember]
+        public string C_CodTipo { get; set; }
+        [DataMember]
+        public bool B_Estado { get; set; }
+
+        [DataMember]
+        public ResidenteBE objResidente { get; set; }
+    }
+}
