9bb8ffdfc4798bd671dac72ff4ab56262c4047f4
llp.lozano <llp.lozano@gmail.com>
2014-05-30 05:17:54 +0000

    DBMLHorarios Avance HorarioDAO

---
+++ ARCHITECTSCONDOMINIO/Architecs.HorariosService/Persitencia/ConexionUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Architecs.HorariosService.Persitencia
+{
+    public class ConexionUtil
+    {
+        public static string CadenaConeccion()
+         
+        {
+            return "workstation id=CondominiosDB.mssql.somee.com;packet size=4096;user id=julioanyosa_SQLLogin_1;pwd=spnnkydfl1;data source=CondominiosDB.mssql.somee.com;persist security info=False;initial catalog=CondominiosDB";
+        }
+        
+    }
+}
--- ARCHITECTSCONDOMINIO/Architecs.HorariosService/Persitencia/HorarioDAO.cs
+++ ARCHITECTSCONDOMINIO/Architecs.HorariosService/Persitencia/HorarioDAO.cs
@@ -2,13 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Collections.Generic;
+using System.Configuration;
 using Architects.Dominio;
 
 namespace Architecs.HorariosService.Persitencia
 {
     public class HorarioDAO
     {
-       
+        private string conexion = string.Empty;
+        public HorarioDAO()
+        {
+            conexion = ConexionUtil.CadenaConeccion();
+        }
+        /*public List<HorarioBE> Listar(string prm_C_Periodo)
+        {
+            List<HorarioBE> lstHorario = new List<HorarioBE>();
+            try
+            {
+                using (DBMLHorariossDataContext SQLDC = new DBMLHorariossDataContext(conexion))
+                {
+                    var resul = SQLDC.dsd_mnt_S_Cuota(prm_C_Periodo);
+
+                    foreach (var item in resul)
+                    {
+                        Cuota cuota = new Cuota();
+                        cuota.N_IdCuota = item.N_IdCuota;
+                        cuota.C_Periodo = item.C_Periodo;
+                        cuota.N_IdVivienda = item.N_IdVivienda;
+                        lstHorario.Add(cuota);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return lstHorario;
+        }*/
         
     }
 }
