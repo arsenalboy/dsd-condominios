d1e2641696f5b4d8f9da8152ef454525b964a2ef
omcr1905@gmail.com <omcr1905@gmail.com@80b915e4-f584-5d82-7bb2-b4c29f7e1674>
2014-06-10 04:25:43 +0000

    Se cambio las referencia para el test

---
+++ trunk/ARCHITECTSCONDOMINIO/Architecs.TestProject/Entidades/QuejaConsulta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace Architecs.TestProject.Dominio
+{
+    [DataContract]
+    public class QuejaConsulta
+    {
+        [DataMember]
+        public int N_IdQueja { get; set; }
+        [DataMember]
+        public int N_IdResidente { get; set; }
+        [DataMember]
+        public string C_Tipo { get; set; }
+        [DataMember]
+        public string C_Motivo { get; set; }
+        [DataMember]
+        public string C_Detalle { get; set; }
+        [DataMember]
+        public DateTime D_FecRegistro { get; set; }
+        [DataMember]
+        public Boolean B_Estado { get; set; }
+        [DataMember]
+        public string C_Nombre { get; set; }
+        [DataMember]
+        public string C_NumDocume { get; set; }
+        [DataMember]
+        public string D_FecQueja { get; set; }
+    }
+}
