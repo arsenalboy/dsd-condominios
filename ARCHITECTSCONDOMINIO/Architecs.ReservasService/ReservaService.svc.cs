092fc59b1064d57b06c9fbe54fd5be9fbe112b58
julio.anyosa <julio.anyosa@gmail.com>
2014-06-13 03:45:44 +0000

    Test de colas

--- ARCHITECTSCONDOMINIO/Architecs.ReservasService/ReservaService.svc.cs
+++ ARCHITECTSCONDOMINIO/Architecs.ReservasService/ReservaService.svc.cs
@@ -16,7 +16,42 @@ namespace Architecs.ReservasService
 
         public ReservasBE CreaReserva(ReservasBE ObjReserva)
         {
-            return ObjReservaDAO.CrearReserva(ObjReserva);
+            try
+            {
+                //validacion de fecha
+                if (ObjReserva.D_FecRegistro < Convert.ToDateTime(DateTime.Now.ToShortDateString()))
+                {
+                    throw new FaultException("La fecha no puede ser inferior a la fecha actual");
+                }
+                //validacion de fecha
+                if (ObjReserva.N_IdHorarioIni > ObjReserva.N_IdHorarioFin)
+                {
+                    throw new FaultException("La hora inicial no puede ser mayor que la hora final");
+                }
+                //validacion maximo tres horas
+                if ((ObjReserva.N_IdHorarioFin - ObjReserva.N_IdHorarioIni + 1) > 3)
+                {
+                    throw new FaultException("No se puede reservar mas  de tres horas");
+                }
+               //validacion maximo de reservas y hora no disponible
+                string valor;
+                valor = ObjReservaDAO.ValidaReserva(ObjReserva);
+                if (valor == "PASO_MAXIMO")
+                {
+                    throw new FaultException("No puede hacer mas de una reserva por el mismo espacio");
+                }
+                else if (valor == "HORA_NO_DISPONIBLE")
+                {
+                    throw new FaultException("La reserva no se aplica para ese horario , ya que esta ocupado");
+                }
+                else
+                    return ObjReservaDAO.CrearReserva(ObjReserva);
+            }
+            catch (Exception ex)
+            {
+                 throw new FaultException(ex.Message);
+            }
+
         }
 
 
---
+++ ARCHITECTSCONDOMINIO/Architecs.Test/TestQuejaMessage.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Messaging;
+using Architecs.Test.Entidades;
+
+namespace Architecs.Test
+{
+    /// <summary>
+    /// Descripción resumida de TestQuejaMessage
+    /// </summary>
+    [TestClass]
+    public class TestQuejaMessage
+    {
+
+        [TestMethod]
+        public void MensajeEncola()
+        {
+            //guardamos los mensajes en cola si ocurre error
+            string rutaCola = @".\private$\queja";
+            if (!MessageQueue.Exists(rutaCola))
+            {
+                MessageQueue.Create(rutaCola);
+            }
+            MessageQueue cola = new MessageQueue(rutaCola);
+            Message mensaje = new Message();
+            mensaje.Label = "Queja registrada con fecha " + DateTime.Now.ToShortDateString();
+            mensaje.Body = new  Queja()
+            {
+                N_IdResidente = 2,
+                B_Estado = false,
+                C_Detalle = "detalle",
+                C_Motivo = "motivo",
+                C_Tipo = "Grave",
+                D_FecQueja = "2014-06-12",
+                D_FecRegistro = new DateTime(10,10,10,10,10,10)
+            };
+            cola.Send(mensaje);
+            
+            //recuperamos el mensaje
+            MessageQueue cola2 = new MessageQueue(rutaCola);
+
+            int cantidadmensajes = 0;
+
+            if (MessageQueue.Exists(rutaCola))
+            {
+                cantidadmensajes = cola2.GetAllMessages().Count();
+                //recorrer y grabar
+                foreach (Message mensajeTodo in cola2.GetAllMessages())
+                {
+                    mensajeTodo.Formatter = new XmlMessageFormatter(new Type[] { typeof(Queja) });
+                    Queja ObjQueja = (Queja)mensajeTodo.Body;
+                    //buscamos el que se envio
+                    if (ObjQueja.N_IdResidente == 2 & ObjQueja.D_FecQueja == "2014-06-12")
+                    {
+                        Assert.AreEqual(false, ObjQueja.B_Estado);
+                        Assert.AreEqual("detalle", ObjQueja.C_Detalle);
+                        Assert.AreEqual("motivo", ObjQueja.C_Motivo);
+                        Assert.AreEqual("Grave", ObjQueja.C_Tipo);
+                        Assert.AreEqual(new DateTime(10, 10, 10, 10, 10, 10), ObjQueja.D_FecRegistro);
+                        cola2.Receive();
+                        break;
+                    }
+
+                }
+                
+                //se elimina los mensajes
+
+                MessageQueue.Delete(rutaCola);
+            }
+            Assert.AreNotEqual(0, cantidadmensajes);
+        }
+    }
+}
