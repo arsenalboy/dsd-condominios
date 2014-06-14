dd05aaac21ed15a97697ce565ac8a48c9e1bce01
llp.lozano@gmail.com <llp.lozano@gmail.com@80b915e4-f584-5d82-7bb2-b4c29f7e1674>
2014-06-14 01:55:53 +0000

    Test de Residentes Agregado

---
+++ trunk/ARCHITECTSCONDOMINIO/Architecs.Test/TestResidentes.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Architecs.Test.WSResidente;
+using System.ServiceModel;
+
+namespace Architecs.Test
+{
+    [TestClass]
+    public class TestResidentes
+    {
+        WSResidente.ResidenteServiceClient client = new WSResidente.ResidenteServiceClient();
+        
+        [TestMethod]
+        public void Test_ListarResidente()
+        {
+            List<ResidenteBE> residentes = new List<ResidenteBE>(client.ListarResidentes());
+            Assert.AreNotEqual(residentes, null);
+        }
+
+        [TestMethod]
+        public void Test_ObtenerResidentePorNumeroDocumento()
+        {
+            string strNroDoc = "88887777";
+            ResidenteBE residente = client.ObtenerResidentePorNroDocumento(strNroDoc);
+            Assert.AreEqual(residente.C_NumDocume, strNroDoc);
+        }
+
+        [TestMethod]
+        public void Test_BuscarResidente()
+        {
+            string strNroDoc = "88887777";
+            List<ResidenteBE> residentes = new List<ResidenteBE>(client.BuscarResidentes("","",strNroDoc));
+            Assert.AreNotEqual(residentes.Count, 0);
+        }
+
+        [TestMethod]
+        public void Test_CrearResidente()
+        {
+            ResidenteBE nuevoResidente = new ResidenteBE();
+            nuevoResidente.C_Nombre = "Selena Demi";
+            nuevoResidente.C_Apellidos = "Gomez Lovato";
+            nuevoResidente.N_TipoDoc = 1;
+            nuevoResidente.C_NumDocume = "83244401";
+            nuevoResidente.C_Correo = "selena@gmail.com";
+            nuevoResidente.C_Clave = "StartDance14";
+            nuevoResidente.D_FecNacimi = new DateTime(1992, 07, 22);
+
+            int idResidente = client.CrearResidente(nuevoResidente);
+            Assert.AreNotEqual(client.ObtenerResidentePorID(idResidente), null);
+        }
+
+        [TestMethod]
+        public void Test_ActualizaResidente()
+        {
+            ResidenteBE modificarResidente = client.ObtenerResidentePorID(1);
+            modificarResidente.C_Correo = "justindelabarra@gmail.com";
+
+            client.ActualizarResidente(modificarResidente);
+
+            Assert.AreEqual(client.ObtenerResidentePorID(1).C_Correo,
+                modificarResidente.C_Correo);
+        }
+
+        [TestMethod]
+        public void Test_EliminarResidente()
+        {
+            try
+            {
+                ResidenteBE eliminarResidente = client.ObtenerResidentePorID(4);
+
+                if (eliminarResidente != null)
+                    client.EliminarResidente(eliminarResidente.N_IdResidente);
+                else
+                    throw new FaultException<ValidationException>
+                       (new ValidationException { ValidationError = "Residente no existe" },
+                       new FaultReason("Validation Failed"));
+
+                Assert.AreEqual(client.ObtenerResidentePorID(4), null);
+            }
+            catch (FaultException<ValidationException> ex)
+            {
+                Console.WriteLine(ex.Detail.ValidationError);
+            }
+        }
+            
+        }
+
+       
+    }
+
